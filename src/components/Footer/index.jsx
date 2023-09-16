63a913ccf1e6cedfbea111cf512ddc10621f54b7
GabrielDoms <gabrieldoms@hotmail.com>
2023-09-16 13:27:29 -0300

    Comitado Fotter

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import { FooterContainer, FooterCopyright } from "./styles";
+
+export function Footer() {
+  return (
+    <FooterContainer>
+      <FooterCopyright>© Copyright Solving O2 - 2023</FooterCopyright>
+    </FooterContainer>
+  );
+}
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export const FooterContainer = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: linear-gradient(to right, #1e40af, #3730a3);
+
+  padding: 2rem 8rem;
+
+  width: 100%;
+`
+
+export const FooterCopyright = styled.span`
+  font-size: 1rem;
+  font-weight: 600;
+
+  color: #ffffff
+`
