389c9812aa66e6b232c994dfcd44e0ca35328ab9
GabrielDoms <gabrieldoms@hotmail.com>
2023-09-16 13:27:11 -0300

    Comitado HEADER

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,50 @@
+import {
+  HeaderContainer,
+  HeaderNavigation,
+  HeaderNavigationLink,
+  LogoContainer,
+  WorkTitle,
+} from "./styles";
+import { Logo } from "../Logo";
+
+const NAV_LINKS = [
+  {
+    title: "Home",
+    to: "/",
+  },
+  {
+    title: "Problema",
+    to: "/problema",
+  },
+  {
+    title: "Solução",
+    to: "/solucao",
+  },
+  {
+    title: "Sobre",
+    to: "/sobre",
+  },
+  {
+    title: "Contato",
+    to: "/contato",
+  },
+];
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <LogoContainer>
+        <Logo />
+        <WorkTitle>Solving o2</WorkTitle>
+      </LogoContainer>
+
+      <HeaderNavigation>
+        {NAV_LINKS.map((route, index) => (
+          <HeaderNavigationLink key={`route-${index}`} to={route.to}>
+            {route.title}
+          </HeaderNavigationLink>
+        ))}
+      </HeaderNavigation>
+    </HeaderContainer>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background: linear-gradient(to right, #1e40af, #3730a3);
+
+  padding: 2rem 8rem;
+
+  width: 100%;
+
+  @media screen and (max-width: 768px) {
+    padding: 2rem 1.5rem;
+  }
+`
+
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  column-gap: 1rem;
+`
+
+export const WorkTitle = styled.span`
+  color: white;
+  
+  font-size: 2rem;
+  font-weight: 600;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1rem;
+  }
+`
+
+export const HeaderNavigation = styled.nav`
+  display: flex;
+  align-items: center;
+
+  column-gap: 1rem;
+`
+
+export const HeaderNavigationLink = styled(Link)`
+  color: white;
+
+  font-size: 1rem;
+  font-weight: 500;
+
+  @media screen and (max-width: 768px) {
+    font-size: 0.875rem;
+  }
+`
